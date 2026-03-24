6c7a73f91ad2e9ab1daef147640e17db4a86428c
artur <artur.arciuch@gmail.com>
2026-03-24 17:21:21 +0100

    first commit

---
+++ build/CMakeFiles/ScreenMouseKeyboard.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for ScreenMouseKeyboard.
+# This may be replaced when dependencies are built.
---
+++ build/CMakeFiles/io.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/io.dir/src/Screen.cpp.o"
+  "CMakeFiles/io.dir/src/Screen.cpp.o.d"
+  "CMakeFiles/io.dir/src/main.cpp.o"
+  "CMakeFiles/io.dir/src/main.cpp.o.d"
+  "io"
+  "io.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/io.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
