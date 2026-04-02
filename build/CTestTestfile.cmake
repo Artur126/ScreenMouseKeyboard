180c4c742937ab3729ea02c32864024dd04f1f17
artur <artur.arciuch@gmail.com>
2026-04-02 12:25:37 +0200

    Remove build directory from repo

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/artur/Projekty/ScreenMouseKeyboard
-# Build directory: C:/Users/artur/Projekty/ScreenMouseKeyboard/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
